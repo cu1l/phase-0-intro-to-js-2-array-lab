863ec99d012f56a996f3cb9acd06b94ff597238c
jakevancleave <jakevancleave1@gmail.com>
2023-01-15 20:56:42 -0800

    commit tree 2becf0406668b749b736cdffde16713f74554db4

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+const catsTwo = [...cats];
+const catsThree = [...cats];
+const catsFour = [...cats];
+const catsFive = [...cats];
+
+function destructivelyAppendCat(name){
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+
+function appendCat(name){
+    catsTwo.push(name);
+    return catsTwo;
+}
+
+function prependCat(name){
+    catsThree.unshift(name);
+    return catsThree;
+}
+
+function removeLastCat(){
+    catsFour.pop();
+    return catsFour;
+}
+
+function removeFirstCat(){
+    catsFive.shift();
+    return catsFive;
+}
